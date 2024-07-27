84b1b228ab6ef93f1b3071cd9fa9cb1ff2699d87
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-07-27 10:48:27 -0400

    add form

--- src/components/create-ability-form.tsx
+++ src/components/create-ability-form.tsx
@@ -1,5 +1,17 @@
 const CreateAbilityForm = () => {
-  return <div>CreateAbilityForm</div>;
+  return (
+    <div className="flex flex-col items-center gap-3 mt-4">
+      <h1 className="font-bold text-2xl">Create a New Ability</h1>
+      <form className="flex items-center gap-3">
+        <label htmlFor="title">Ability Id</label>
+        <input type="text" className="rounded-sm border border-gray-600" />
+      </form>
+      <form className="flex items-center gap-3">
+        <label htmlFor="title">Ability Name</label>
+        <input type="text" className="rounded-sm border border-gray-600" />
+      </form>
+    </div>
+  );
 };
 
 export default CreateAbilityForm;
